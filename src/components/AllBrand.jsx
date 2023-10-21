70d46f58da1de4850c75e25aadf75e78d5f8e1c9
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2023-10-21 16:11:35 +0600

    edit the readme file

--- src/components/AllBrand.jsx
+++ src/components/AllBrand.jsx
@@ -3,7 +3,7 @@ import BrandCategory from "./BrandCategory";
 const AllBrand = ({carBrands}) => {
     console.log(carBrands);
     return (
-        <div className="md:py-20 py-12 mx-4 md:mx-0">
+        <div className="md:py-20 py-12 mx-4 md:mx-0 md:px-12 lg:px-0">
             <div className="text-center font-kanit">
                 <h1 className="font-bold text-4xl md:text-6xl pb-8 px-12"><span className="text-[#45D792]">Search</span> Your Dream Car</h1>
                 <p className="md:text-xl mb-12">There has 6+ Brand Category Available</p>
